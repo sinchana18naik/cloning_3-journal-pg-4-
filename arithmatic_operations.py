c268356608d5477c1dcfdcb97d632861c9438b4b
sinchana18naik <sinchananaik112005@gmail.com>
2025-10-23 14:06:28 +0530

    Create arithmatic_operations.py

---
+++ arithmatic_operations.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the 1st number"))
+num2=int(input("enter the 2nd number"))
+add=num1+num2
+sub=num1-num2
+print("arithmatic operations")
+print(f"addition of {num1} and {num2} is",add)
+print(f"subtraction of {num1} and {num2} is",sub)
