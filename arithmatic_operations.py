b35f70d947d0ffbb7cffefe414e4842d92085500
sinchana18naik <sinchananaik112005@gmail.com>
2025-10-23 14:17:04 +0530

    save to local branch

--- arithmatic_operations.py
+++ arithmatic_operations.py
@@ -2,6 +2,10 @@ num1=int(input("enter the 1st number"))
 num2=int(input("enter the 2nd number"))
 add=num1+num2
 sub=num1-num2
+multi=num1*num2
+div=num1/num2
 print("arithmatic operations")
 print(f"addition of {num1} and {num2} is",add)
 print(f"subtraction of {num1} and {num2} is",sub)
+print(f"multiplication of {num1} and {num2} is",multi)
+print(f"division of {num1} and {num2} is",div)
